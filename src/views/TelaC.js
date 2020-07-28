515821512f996d72b1f17ceae9987c1a7b108900
Anthony Cruz <anthonyleierlw@gmail.com>
2020-07-27 21:05:03 -0300

    Navegação Stack

--- src/views/TelaC.js
+++ src/views/TelaC.js
@@ -1,4 +1,7 @@
 import React from 'react';
 import TextoCentral from '../components/TextoCentral';
 
-export default props => <TextoCentral corFundo="#9932cd">Tela C</TextoCentral>;
+export default props => {
+	const route = props.route || {params: {numero: 0}};
+	return <TextoCentral corFundo="#9932cd">Tela C com {route.params.numero}</TextoCentral>;
+};
