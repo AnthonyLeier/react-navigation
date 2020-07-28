00aaf13dec148631342d21bca00e7b5ea9e7c7c8
Anthony Cruz <anthonyleierlw@gmail.com>
2020-07-28 13:32:35 -0300

    Stack, Drawer e Icons

--- src/views/TelaC.js
+++ src/views/TelaC.js
@@ -2,6 +2,7 @@ import React from 'react';
 import TextoCentral from '../components/TextoCentral';
 
 export default props => {
-	const route = props.route || {params: {numero: 0}};
-	return <TextoCentral corFundo="#9932cd">Tela C com {route.params.numero}</TextoCentral>;
+	const params = props.route.params;
+	const numero = params ? params : 0;
+	return <TextoCentral corFundo="#9932cd">Tela C com {numero}</TextoCentral>;
 };
